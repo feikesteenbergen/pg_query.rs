4e5838b06809f83e15e8a69cd4dbcb32e235439e
Feike Steenbergen <feike@timescale.com>
2025-02-03 16:04:24 +0100

    Include serialize feature
    For inspecting statements and storing them (in PostgreSQL as json for
    example) it is sometimes useful to serialize the parsed query.
    
    This feature flag allows this to happen.

--- src/node_enum.rs
+++ src/node_enum.rs
@@ -1,8 +1,11 @@
 use crate::*;
 
 pub use protobuf::node::Node as NodeEnum;
+use serde::Serialize;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+#[cfg(feature = "serialize")]
+#[derive(Serialize)]
 pub enum Context {
     None,
     Select,
